de1b514a8e997121d7251e9813477a5b56887e17
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2021-01-19 05:24:35 +0300

    clone big-hn-threads

---
+++ code.js
@@ -0,0 +1,46 @@
+async function get(id) {
+  return await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+  ).then(r => r.json());
+}
+
+async function pull(id, descendants) {
+  window.pullCallback && window.pullCallback(descendants);
+  const node = await get(id);
+
+  const children = [];
+  let value = 1;
+
+  const pulledKids = await Promise.all(
+    ((node && node.kids) || []).map(k =>
+      pull(k, node.descendants || descendants)
+    )
+  );
+
+  let data = {};
+
+  for (k of pulledKids) {
+    // k.parent = data;
+    children.push(k);
+    value += k.value;
+  }
+
+  data.id = id;
+  data.name = node ? node.by : '?';
+  data.value = value;
+  data.text = node ? node.text || node.title || node.url : '?';
+  data.children = children;
+
+  if (children.length === 0) {
+    delete data.children;
+  }
+
+  return data;
+}
+
+async function start(id) {
+  const data = await pull(id);
+
+  // fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8');
+  return data;
+}
